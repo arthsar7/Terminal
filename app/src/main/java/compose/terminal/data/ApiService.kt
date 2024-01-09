c7c1cc18fc5be5bff59d4b1539b8d8c7503ea5d7
Arthur Deverson <arturosardaryan@yandex.ru>
2024-01-09 19:18:53 +0300

    Adding TransformableState

--- app/src/main/java/compose/terminal/data/ApiService.kt
+++ app/src/main/java/compose/terminal/data/ApiService.kt
@@ -3,6 +3,6 @@ package compose.terminal.data
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=GS9QXH1xhzSTm0nsazTQbyf0WqJeqpR4")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=GS9QXH1xhzSTm0nsazTQbyf0WqJeqpR4")
     suspend fun loadBars(): Result
 }
--- app/src/main/java/compose/terminal/presentation/Terminal.kt
+++ app/src/main/java/compose/terminal/presentation/Terminal.kt
@@ -2,26 +2,43 @@ package compose.terminal.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import compose.terminal.data.Bar
+import kotlin.math.roundToInt
 
+private const val VISIBLE_BARS_MIN_VALUE = 20
+private const val START_VISIBLE_BARS_COUNT = 100
 @Composable
 fun Terminal(bars: List<Bar>) {
+    var visibleBarsCount by remember {
+        mutableIntStateOf(START_VISIBLE_BARS_COUNT)
+    }
+    val transformableState = TransformableState { zoomChange, panChange, rotationChange ->
+        val zoomVal = (visibleBarsCount / zoomChange).roundToInt()
+        visibleBarsCount = zoomVal.coerceIn(VISIBLE_BARS_MIN_VALUE, bars.size)
+    }
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .transformable(transformableState)
     ) {
         val max = bars.maxOf { it.high }
         val min = bars.minOf { it.low }
         val pxPerPoint = size.height / (max - min)
-        val barWidth = size.width / bars.size
-        bars.forEachIndexed { index, bar ->
-            val offsetX = index * barWidth
+        val barWidth = size.width / visibleBarsCount
+        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
+            val offsetX = size.width - index * barWidth
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
