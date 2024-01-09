beb7f0227283fffd74dbd31331949bc8a7fda465
Arthur Deverson <arturosardaryan@yandex.ru>
2024-01-10 02:32:34 +0300

    Implementing drawPrices with path effect

--- app/src/main/java/compose/terminal/presentation/Terminal.kt
+++ app/src/main/java/compose/terminal/presentation/Terminal.kt
@@ -5,18 +5,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.dp
 import compose.terminal.data.Bar
 import kotlin.math.roundToInt
 
 private const val VISIBLE_BARS_MIN_VALUE = 20
+
 @Composable
 fun Terminal(bars: List<Bar>) {
     var terminalState by rememberTerminalState(bars)
@@ -34,13 +39,15 @@ fun Terminal(bars: List<Bar>) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .padding(32.dp)
             .transformable(transformableState)
             .onSizeChanged {
                 terminalState = terminalState.copy(terminalWidth = it.width.toFloat())
             }
     ) {
         val visibleMax = terminalState.visibleBars.maxOf { it.high }
-        val visibleMin = terminalState.visibleBars.take(terminalState.visibleBarsCount).minOf { it.low }
+        val visibleMin =
+            terminalState.visibleBars.take(terminalState.visibleBarsCount).minOf { it.low }
         val pxPerPoint = size.height / (visibleMax - visibleMin)
         translate(left = terminalState.scrolledBy) {
             bars.forEachIndexed { index, bar ->
@@ -59,5 +66,47 @@ fun Terminal(bars: List<Bar>) {
                 )
             }
         }
+        bars.firstOrNull()?.let {
+            drawPrices(
+                min = visibleMin,
+                pxPerPoint = pxPerPoint,
+                lastPrice = it.close
+            )
+        }
     }
+}
+
+private fun DrawScope.drawPrices(
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float
+) {
+    drawDashedLine(
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+    )
+    drawDashedLine(
+        start = Offset(0f, size.height - (lastPrice - min) * pxPerPoint),
+        end = Offset(size.width, size.height - (lastPrice - min) * pxPerPoint),
+    )
+    drawDashedLine(
+        start = Offset(0f, size.height),
+        end = Offset(size.width, size.height),
+    )
+}
+
+private fun DrawScope.drawDashedLine(
+    color: Color = Color.White,
+    start: Offset,
+    end: Offset,
+    strokeWidth: Float = 1f,
+    intervals : FloatArray = floatArrayOf(4.dp.toPx(), 4.dp.toPx()),
+) {
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        pathEffect = PathEffect.dashPathEffect(intervals)
+    )
 }
